3f3266372a6d4265fedde4a8c97e19424c11d17e
alpheratz0 <alpheratz99@protonmail.com>
2023-08-03 09:51:48 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -2,10 +2,8 @@
 # This program is free software.
 
 VERSION=0.1.2
-
 CC=cc
 CFLAGS=-ansi -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
 LDFLAGS=-s
-
 PREFIX=/usr/local
 MANPREFIX=$(PREFIX)/share/man
