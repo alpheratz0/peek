1b3d5f156663acfe8d91f5353b70406079f8ec3d
alpheratz0 <alpheratz99@protonmail.com>
2023-09-27 22:15:39 -0300

    clean up config.mk

--- config.mk
+++ config.mk
@@ -1,9 +1,12 @@
 # Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION=0.1.2
-CC=cc
-CFLAGS=-ansi -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
-LDFLAGS=-s
-PREFIX=/usr/local
-MANPREFIX=$(PREFIX)/share/man
+VERSION = 0.1.2
+
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+CFLAGS = -ansi -Os -DVERSION=\"$(VERSION)\"
+LDFLAGS = -s
+
+CC = cc
